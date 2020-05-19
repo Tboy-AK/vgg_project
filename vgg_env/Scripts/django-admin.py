5996cf016ded82db42d09ed939d70831917019d8
Tobi Akanji <tobia807@gmail.com>
2020-05-19 04:49:22 +0100

    ch(gitignore):initiate with gitignore and README

--- vgg_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\AKANJI OLUWATOBILOBA\Downloads\vgg-virtual-internship\vgg_project\vgg_env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- vgg_food_vendor_project/food_vendor_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FoodVendorAppConfig(AppConfig):
-    name = 'vgg_food_vendor_project.food_vendor_app'
