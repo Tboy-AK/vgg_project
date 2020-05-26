fc28670f23a04dc76c02c9ff1cd84be038d9c4f7
Tobi Akanji <tobia807@gmail.com>
2020-05-26 22:52:49 +0100

    Include media files in settings

--- vgg_food_vendor_project/settings.py
+++ vgg_food_vendor_project/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': (
